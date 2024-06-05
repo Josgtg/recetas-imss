fda946668d1fcafd154bedbf574ac1f73a6c124a
Josué <josue.ger.torres.gar@gmail.com>
2024-06-05 14:01:45 -0600

    Se añadieron varios archivos html

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+header.innerHTML = `
+<div class="container-fluid bg-primary">
+    <div class="container">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+            <img src="assets/img/logoIMSSlight.png" alt="" width="45">
+            <button
+                class="navbar-toggler d-lg-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            ></button>
+            </div>
+        </nav>
+    </div>
+</div>
+`;
+
+footer.innerHTML = `
+    <small>universidad autonoma de zacatecas</small>
+`;
